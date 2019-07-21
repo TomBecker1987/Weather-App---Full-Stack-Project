30f95827cb62c378b79cf21ba0dcaaee2b47fe7d
Tom Becker <beckertomarch@gmail.com>
2019-07-21 14:13:05 +0300

    change directory structure

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const request = require('request');
-const api = require('./routes/api')
+const api = require('./server/routes/api')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/weatherDB', {useNewUrlParser: true})
 
 
-app.use(express.static(path.join(__dirname,'..', 'dist')))
+app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
